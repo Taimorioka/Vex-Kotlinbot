22fce1df262cd87a16051d7b97fb9d33511c680f
taiMorioka <thetaimorioka@gmail.com>
2024-12-06 15:28:23 -0800

    did some fixes, added elbow

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("../Hydrozoa-Java/build/staging-deploy")
+    }
 }
 
 dependencies {
-    implementation("dev.vexide:hydrozoa:0.1.0-alpha.2")
+    implementation("dev.vexide:hydrozoa:0.1.0-alpha.3")
 }
 
 kotlin {
--- src/main/kotlin/Robot.kt
+++ src/main/kotlin/Robot.kt
@@ -13,15 +13,27 @@ import dev.vexide.hydrozoa.devices.smart.MotorControl.Voltage
 
 private const val GEAR_RATIO = 7
 private const val ARM_SPEED = Int.MAX_VALUE
-private val ARM_UP = EncoderPosition.ofDegrees(180.0 * GEAR_RATIO)
-private val ARM_DOWN = EncoderPosition.ofDegrees(1.0 * GEAR_RATIO)
+private val ARM_UP = EncoderPosition.ofDegrees(110.0 * GEAR_RATIO)
+private val ARM_DOWN = EncoderPosition.ofDegrees(0.0 * GEAR_RATIO)
+private val ARM_MID = EncoderPosition.ofDegrees(52.0 * GEAR_RATIO)
+private val ElBOW_UP = EncoderPosition.ofDegrees(90.0 * GEAR_RATIO)
+private val ELBOW_DOWN = EncoderPosition.ofDegrees(0.0 * GEAR_RATIO)
+private val ELBOW_MID = EncoderPosition.ofDegrees(45.0 * GEAR_RATIO)
+
 
 class Robot(peripherals: Peripherals):  CompetitionRobot{
-    var left_Drive = Motor(peripherals.takePort(1), Motor.Gearset.Red, Motor.Direction.FORWARD)
-    var right_Drive = Motor(peripherals.takePort(0), Motor.Gearset.Red, Motor.Direction.FORWARD)
-    var arm = Motor(peripherals.takePort(2), Motor.Gearset.Red, Motor.Direction.FORWARD)
-    var claw = Motor(peripherals.takePort(3), Motor.Gearset.Red, Motor.Direction.FORWARD)
+    var right_Drive = Motor(peripherals.takePort(1), Motor.Gearset.Red, Motor.Direction.FORWARD)
+    var left_Drive = Motor(peripherals.takePort(2), Motor.Gearset.Red, Motor.Direction.FORWARD)
+    var arm = Motor(peripherals.takePort(3), Motor.Gearset.Red, Motor.Direction.FORWARD)
+    var elbow = Motor(peripherals.takePort(4), Motor.Gearset.Red, Motor.Direction.REVERSE)
+    var claw = Motor(peripherals.takePort(5), Motor.Gearset.Red, Motor.Direction.FORWARD)
     var controller = peripherals.takeController(Controller.Id.Primary)
+    override fun driverInit()  {
+        elbow.target = Brake(BrakeMode.BRAKE)
+        arm.target = Brake(BrakeMode.BRAKE)
+    }
+
+
     override fun driverPeriodic() {
 
         val input = controller.state.orElseGet(Controller.State::empty)
@@ -37,19 +49,26 @@ class Robot(peripherals: Peripherals):  CompetitionRobot{
             right_Drive.target = Voltage(rightVoltage)
 
             if (input.r2.pressed) {
-                claw.target = Voltage(6.0)
+                claw.target = Voltage(10.0)
             } else if (input.l2.pressed) {
-                claw.target = Voltage(-6.0)
+                claw.target = Voltage(-10.0)
             } else {
                 claw.target = Brake(BrakeMode.HOLD)
             }
 
-            if (input.r1.isNowPressed) {
-                arm.target = MotorControl.Position(ARM_UP, ARM_SPEED)
+            if (input.b.isNowPressed) {
+                arm.target = MotorControl.Position(ARM_DOWN, ARM_SPEED)
+                elbow.target = MotorControl.Position(ELBOW_DOWN, ARM_SPEED)
             }
 
-            if (input.l1.isNowPressed) {
-                arm.target = MotorControl.Position(ARM_DOWN, ARM_SPEED)
+            if (input.a.isNowPressed) {
+                arm.target = MotorControl.Position(ARM_MID, ARM_SPEED)
+                elbow.target = MotorControl.Position(ELBOW_MID, ARM_SPEED)
+            }
+
+            if (input.x.isNowPressed) {
+                arm.target = MotorControl.Position(ARM_UP, ARM_SPEED)
+                elbow.target = MotorControl.Position(ElBOW_UP, ARM_SPEED)
             }
 
         } catch (err: DeviceException) {
